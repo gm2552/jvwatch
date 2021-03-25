0cd4d84d0584fafa59b9dbf9ac66df28c48c4348
gm2552 <gm2552@xmppserver.cernerdirectsupport.com>
2021-03-25 11:20:38 -0500

    Removing EnableScheduling annotation and cleaning up feign annotation.

--- src/main/java/abareaso/io/jvwatch/JVWatachApplication.java
+++ src/main/java/abareaso/io/jvwatch/JVWatachApplication.java
@@ -5,13 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 @SpringBootApplication
-@EnableReactiveFeignClients("abareaso.io.jvwatch.feign")
-@EnableScheduling
+@EnableReactiveFeignClients
 @EnableRedisRepositories
 @EnableConfigurationProperties
 public class JVWatachApplication
